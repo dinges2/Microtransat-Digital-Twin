0d71677f6b96a3cd8591fb5c2b2e5627d6029df5
Mike Schouten <mike@Mikes-MacBook-Pro.local>
2021-03-30 14:55:00 +0200

    potmeter aangepast aan simulatie

--- potmeter/potmeter.py
+++ potmeter/potmeter.py
@@ -1,16 +1,22 @@
-import serial
+from Data import *
 
-#arduino object with port, baud rate and timeout specified
-arduino = serial.Serial('COM4',9600,timeout=.1)
+class arduinoInputData:
+    def __init__(self):
+        self.target_sail_angle = 0
+        self.target_gimbal_rudder_angle = angle()
+        #arduino object with port, baud rate and timeout specified
+        arduino = serial.Serial('COM4',9600,timeout=.1)
 
-def angle():
-    while(True):
+    def angle(self):
         data = arduino.readline()[:-1]
         #decode the data
         decoded_data = str(data,'utf-8')
         print(decoded_data)
         #grabs the first 3 symbols
-    return decoded_data
+        return decoded_data
 
-
-angle()
+    def getTargetSailAngle(self):
+        return 0
+    
+    def getTargetRudderAngle(self):
+        return angle()
